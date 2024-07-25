93e013465ce2cd9a66a572eaa548bb4b0b8e26d1
iqra azam <iqraazam7355@gmail.com>
2024-07-26 02:06:37 +0530

    pushing mega blog

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo({width = '100px'}){
+    return(
+       
+    //     <div>logo</div>
+    <img className="w-10 rounded-xl" src="img1.jpg" alt="" />
+    )
+}
+
+export default Logo
